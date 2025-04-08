1fb2d96e19269aecddb4c46d95f2cc72438744e7
KenzDev <alzzoffchosting@gmail.com>
2025-04-08 19:40:01 +0700

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,9 +3,9 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Velyn</div>
+      <div className={styles.logo}>Kenz</div>
       <div className={styles.text}>
-        VelynTeam © All rights reserved 2025.
+        KenzTeam © All rights reserved 2025.
       </div>
     </div>
   );
